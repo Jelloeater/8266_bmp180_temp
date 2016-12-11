95f169684278196cb769a7ed1cd5f64dfa27b7bf
Jesse <jelloeater@gmail.com>
2016-12-11 14:44:09 -0500

    Started on socket client

---
+++ data_client.py
@@ -0,0 +1,24 @@
+import socket
+import sys
+
+text_in='temp data'
+
+def send(text_in)
+    HOST, PORT = "192.168.1.16", 1337
+    data = text_in.join(sys.argv[1:])
+
+    # Create a socket (SOCK_STREAM means a TCP socket)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # Connect to server and send data
+        sock.connect((HOST, PORT))
+        sock.sendall(bytes(data + "\n", "utf-8"))
+
+        # Receive data from the server and shut down
+        received = str(sock.recv(1024), "utf-8")
+    finally:
+        sock.close()
+
+    print("Sent:     {}".format(data))
+    print("Received: {}".format(received))
