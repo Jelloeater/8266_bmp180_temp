b018dce0be67eed626d08516ae0bb156c9feff2c
Jesse <jelloeater@gmail.com>
2017-05-28 19:06:44 -0400

    Updated changes to I2C code to reflect new firmware https://www.bountysource.com/issues/40213096-driver-for-bme280-broken-since-v1-8-6-77-ga3320e7
    https://forum.micropython.org/viewtopic.php?t=2919

---
+++ client/boot.py
@@ -0,0 +1,6 @@
+import esp
+esp.osdebug(None)
+import gc
+import webrepl
+webrepl.start()
+gc.collect()
--- client/main.py
+++ client/main.py
@@ -4,7 +4,7 @@ __author__ = 'Jesse'
 import json, esp, machine
 from machine import I2C, Pin
 from bmp180 import BMP180
-import socket
+import urequests
 
 SLEEP_TIMEOUT = 10
 HOST, PORT = "192.168.1.16", 8080
@@ -30,36 +30,18 @@ class main:
 
 class data_client:
     @staticmethod
-    def build_payload(data_in):
-        headers = """\
-        POST /auth HTTP/1.1\r
-        Content-Type: {content_type}\r
-        Content-Length: {content_length}\r
-        Host: {host}\r
-        Connection: close\r
-        \r\n"""
-
-        body = 'userName=Ganesh&password=pass'
-        header = headers.format(
-            content_type="application/x-www-form-urlencoded",
-            content_length=len(body),
-            host=str(HOST) + ":" + str(PORT)
-        )
-        payload = ''.join(header + body)
-        print ("Payload: " + payload)
-        return ''.join(payload).encode("utf-8")
-
-    @staticmethod
     def send(data):
-        # Create a socket (SOCK_STREAM means a TCP socket)
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            # Connect to server and send data
-            sock.connect((HOST, PORT))
-            sock.sendall(data_client.build_payload(data))
-        finally:
-            sock.close()
-        print("Sent:     {}".format(data))
+        r = urequests.post("http://192.168.1.16:8080/")
+        print(r)
+        print(r.content)
+        print(r.text)
+        print(r.content)
+        print(r.json())
+
+        # It's mandatory to close response objects as soon as you finished
+        # working with them. On MicroPython platforms without full-fledged
+        # OS, not doing so may lead to resource leaks and malfunction.
+        r.close()
 
 class env_sensor_Info:
     def __init__(self):
@@ -70,7 +52,7 @@ class env_sensor_Info:
 
     def update(self):
         # Returns object with temp, p, altitude
-        bus = I2C(Pin(5), Pin(4), freq=9600)
+        bus = I2C(-1, Pin(5), Pin(4), freq=9600)
         bmp180 = BMP180(bus)
         bmp180.oversample_sett = 2
         bmp180.baseline = 101325
